00a12feaed7e2214b4641b59f04f94e178d82e57
sid <sidvanamala@gmail.com>
2024-02-05 14:42:42 -0800

    Add ball tracking proj

---
+++ src/Filters/BallTrackingProject.java
@@ -0,0 +1,12 @@
+package Filters;
+
+import Interfaces.PixelFilter;
+import core.DImage;
+
+public class BallTrackingProject implements PixelFilter {
+
+    @Override
+    public DImage processImage(DImage image) {
+        return image;
+    }
+}
