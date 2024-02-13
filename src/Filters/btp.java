dc41723971d30314b1cbf187abd7c28b7fd9ba7e
sid <sidvanamala@gmail.com>
2024-02-13 09:47:02 -0800

    Added and modified code for tracking center. Need to test and figure out how to remove clusters at the end of each ball center has been found.

--- src/Filters/btp.java
+++ src/Filters/btp.java
@@ -15,7 +15,7 @@ public class btp implements PixelFilter, Interactive {
     private short green2;
 
     public btp() {
-        threshold = 32;
+        threshold = 35;
         red2 = 245;
         blue2 = 64;
         green2 = 75;
@@ -31,7 +31,7 @@ public class btp implements PixelFilter, Interactive {
         short[][] blue = newImage.getBlueChannel();
 
         // doing the masking
-        doMasking(image, red, green , blue);
+        doMasking(image, red, green, blue);
 
         //Locate center
         findingCenter(red, green, blue);
@@ -48,13 +48,11 @@ public class btp implements PixelFilter, Interactive {
                     red[r][c] = 255;
                     green[r][c] = 255;
                     blue[r][c] = 255;
-                }
-                else if (colorDistance2 <= threshold) {
+                } else if (colorDistance2 <= threshold) {
                     red[r][c] = 255;
                     green[r][c] = 255;
                     blue[r][c] = 255;
-                }
-                else {
+                } else {
                     red[r][c] = 0;
                     green[r][c] = 0;
                     blue[r][c] = 0;
@@ -65,50 +63,75 @@ public class btp implements PixelFilter, Interactive {
     }
 
     private void findingCenter(short[][] red, short[][] green, short[][] blue) {
-        int averageRow = 0, averageCol = 0, Whitepixels = 0;
-        for (int row = 0; row < red.length; row++) {
-            for (int col = 0; col < red[row].length; col++) {
-                if (red[row][col] == 255 && green[row][col] == 255 && blue[row][col] == 255) {
-                    averageRow += row;
-                    averageCol += col;
-                    Whitepixels ++;
-                }
-            }
+        int side = 3; // square size
+        int r = (int) (Math.random() * red.length);
+        int c = (int) (Math.random() * red[0].length);
+        while (red[r][c] != 255 && green[r][c] != 255 && blue[r][c] != 255) {
+            r = (int) (Math.random() * red.length);
+            c = (int) (Math.random() * red[0].length);
         }
-        if (Whitepixels != 0) {
-            averageRow /= Whitepixels;
-            averageCol /= Whitepixels;
-        }
+        printCenter(red, green, blue, r, c, side);
 
-        red[(short)averageRow][(short)averageCol] = 255;
-        green[(short)averageRow][(short)averageCol] = 0;
-        blue[(short)averageRow][(short)averageCol] = 0;
     }
 
+    private void printCenter(short[][] red, short[][] green, short[][] blue, int r, int c, int side) {
+        int prevCenterR = r, prevCenterC = c; // initial position
+        int centerR = prevCenterR + 1, centerC = prevCenterC + 1;
+
+        while (prevCenterC != centerC && prevCenterR != centerR) {
+            int averageR = 0, averageC = 0, countWhites = 0;
+            if (numOfBlacks(red, green, blue) <= side) side++;
+            else {
+                prevCenterR = centerR;
+                prevCenterC = centerC; // putting the current center into the previous center variable to replace the new center
+                changeCenter(red, green, blue, centerR, centerC, side, r, c);
+            }
+        }
+    }
+
+    private int numOfBlacks ( short[][] red, short[][] green, short[][] blue){
+        int numBlacks = 0;
+        for (int r = 0; r < red.length; r++) {
+            for (int c = 0; c < red[0].length; c++) {
+                if ((red[r][c] == 0 && green[r][c] == 0 && blue[r][c] == 0)) numBlacks++;
+            }
+        }
+        return numBlacks;
+    }
     @Override
-    public void mouseClicked(int mouseX, int mouseY, DImage img) {
+    public void mouseClicked ( int mouseX, int mouseY, DImage img){
         red = img.getRedChannel()[mouseY][mouseX];
         green = img.getGreenChannel()[mouseY][mouseX];
         blue = img.getBlueChannel()[mouseY][mouseX];
     }
-
     @Override
-    public void keyPressed(char key) {
+    public void keyPressed ( char key){
         if (key == '+') {
             if (threshold <= 222) {
                 threshold += 5;
             }
         }
-        // wassup
-
         if (key == '-') {
             if (threshold >= 0) {
                 threshold -= 5;
             }
         }
     }
-
-    private short findColorDistance(short r, short g, short b, short red, short blue, short green) {
-        return (short)Math.sqrt( Math.pow(Math.abs(r-red), 2) + Math.pow(Math.abs(b-blue), 2) + Math.pow(Math.abs(g-green), 2));
+    private short findColorDistance ( short r, short g, short b, short red, short blue, short green){
+        return (short) Math.sqrt(Math.pow(Math.abs(r - red), 2) + Math.pow(Math.abs(b - blue), 2) + Math.pow(Math.abs(g - green), 2));
+    }
+    private void changeCenter ( short[][] red, short[][] green, short[][] blue, int centerR, int centerC, int side,int r, int c){
+        int countWhites = 0, averageR = 0, averageC = 0;
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < side; j++) {
+                if ((red[i + r][j + c] == 255 && green[i + r][j + c] == 255 && blue[i + r][j + c] == 255)) {
+                    averageR += i;
+                    averageC += j;
+                    countWhites++;
+                }
+            }
+        }
+        centerR = averageR / countWhites;
+        centerC = averageC / countWhites;
     }
 }
